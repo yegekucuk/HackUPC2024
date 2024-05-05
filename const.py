b3d410d6c277598b4fcaa7d547f3a1ae068ecdfb
yegekucuk <eegekucuk@hotmail.com>
2024-05-05 07:25:56 +0300

    Ready demo
    Added timer, added point, aligned texts to center, removed enemies, added images

--- const.py
+++ const.py
@@ -1,17 +1,26 @@
+from random import randint
+
 # resolution of the game
-res = (1920,1080)
+res = (800,600)
 res_x = res[0]
 res_y = res[1]
 
 # pygame text font
-font_size = 100
-
+font_size = 30
+point_font_size = 30
 
 # properties
-obstacle_size = 30
+goal_size = 30
 character_size = 40
 collision = 0
 acceleration = 10
+time_given = 180
 
 # color
 rgb_white = (255,255,255)
+rgb_black = (0,0,0)
+
+#images
+pieces = 7
+bee_img_loc = f"img/Bee{randint(1,7)}.png"
+flower_img_loc = f"img/Flower1.png"
--- game.py
+++ game.py
@@ -3,6 +3,10 @@ from random import randrange
 import face
 import cv2
 from const import *
+import time
+
+# game point
+point = 0
 
 # start the camera
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -36,17 +40,24 @@ while True:
 
     screen.fill("black")
     text_surface = my_font.render("Click to start game", False, rgb_white)
-    screen.blit(text_surface, (650,500))
+    size_of_text = text_surface.get_size()
+    screen.blit(text_surface, ((res_x//2) - size_of_text[0]//2, (res_y//2) - size_of_text[1]//2))
     pygame.display.flip()
     clock.tick(60)
 
 # random position for obstacle
 pos = pygame.Vector2(randrange(res_x), randrange(res_y))
-goal_rect = pygame.Rect(pos[0], pos[1], character_size*2, character_size*2)
-enemy_rects = []
-enemy_rects.append({pygame.Rect(randrange(res_x), randrange(res_y)), time.time()})
-enemy_rects.append({pygame.Rect(randrange(res_x), randrange(res_y)), time.time()})
+goal_rect = pygame.Rect(pos[0], pos[1], goal_size, goal_size)
+# adding enemy
+    #enemy_rects = []
+    #enemy_rects.append((pygame.Rect(randrange(res_x), randrange(res_y), goal_size*2, goal_size*2), time.time()))
+# bee image load
+image = pygame.image.load(bee_img_loc)
+image2 = pygame.image.load(flower_img_loc)
+image = pygame.transform.scale(image, (character_size, character_size))
+image2 = pygame.transform.scale(image2, (goal_size, goal_size))
 
+start_time = time.time()
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -57,7 +68,17 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("green")
     # creating the character
-    pygame.draw.circle(screen, "blue", player_pos, character_size)
+        #pygame.draw.circle(screen, "blue", player_pos, character_size)
+    screen.blit(image, player_pos - pygame.Vector2(character_size/2, character_size/2))
+
+    time_left = time_given-(time.time()-start_time)
+    if time_left < 0:
+        running = False
+
+    # display point
+    point_font = pygame.font.SysFont('Comic Sans MS', point_font_size)
+    text_surface = point_font.render("Point: %d  Time: %d" % (point,time_left), False, rgb_black)
+    screen.blit(text_surface, (0,0))
     
     change, location = face.getDeltaLoc(location[0], location[1])
     player_movement = face.getDirectionChange(change, player_movement)
@@ -79,19 +100,21 @@ while running:
         else:
             player_pos[1] = 0
 
-    player_rect = pygame.Rect(player_pos[0], player_pos[1], character_size*2, character_size*2)
-    pygame.draw.circle(screen, "red", pos, goal_size)
+    player_rect = pygame.Rect(player_pos[0], player_pos[1], character_size, character_size)
+        #pygame.draw.circle(screen, "red", pos, goal_size)
+    screen.blit(image2, pos - pygame.Vector2(goal_size/2, goal_size/2))
     collision = goal_rect.colliderect(player_rect)
     if collision:
-        pos = pygame.Vector2(randrange(1920), randrange(1080))
-        goal_rect = pygame.Rect(pos[0], pos[1], goal_size*2, goal_size*2)
-    for enemy in enemy_rects:
-        if (time.time() - enemy[1] >= enemy_life):
-            enemy_rects.remove(enemy)
-            enemy_rects.append({pygame.Rect(randrange(res_x), randrange(res_y)), time.time()})
-        collision = enemy[0].colliderect(player_rect)
-        if (time.time() - enemy[1] >= activation_time and collision):
-            running = False
+        pos = pygame.Vector2(randrange(res_x), randrange(res_y))
+        goal_rect = pygame.Rect(pos[0], pos[1], goal_size, goal_size)
+        point += 1
+        #for enemy in enemy_rects:
+        #    if (time.time() - enemy[1] >= enemy_life):
+        #        enemy_rects.remove(enemy)
+        #        enemy_rects.append((pygame.Rect(randrange(res_x), randrange(res_y), goal_size*2, goal_size*2), time.time()))
+        #    collision = enemy[0].colliderect(player_rect)
+        #    if (time.time() - enemy[1] >= activation_time and collision):
+        #        running = False
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -104,7 +127,8 @@ while True:
             pygame.quit()
     screen.fill("black")
     
-    text_surface = my_font.render("GAME OVER", False, rgb_white)
-    screen.blit(text_surface, ((res_x//2)-font_size, (res_y//2)-font_size))
+    text_surface = my_font.render("Your score: %d" %(point), False, rgb_white)
+    size_of_text = text_surface.get_size()
+    screen.blit(text_surface, ((res_x//2) - size_of_text[0]//2, (res_y//2) - size_of_text[1]//2))
     pygame.display.flip()
     clock.tick(60)
